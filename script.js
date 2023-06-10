5da87de8572a7cdf9cdd92ef5b5b326ac27c899f
harshjain56 <127614518+harshjain56@users.noreply.github.com>
2023-06-10 12:33:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let height=document.getElementById("height")
+let width=document.getElementById("width")
+
+
+
+
+function getSize(){
+   
+   let screenWidth=window.innerWidth
+   let screenHeight=window.innerHeight
+   height.innerText=`${screenHeight}`
+   width.innerText=`${screenWidth}`
+
+  
+}
